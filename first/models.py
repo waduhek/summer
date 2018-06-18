f1ec87548d323e6b2df44d6ff922335bbccd2433
Ryan Noronha <ryannor56a@gmail.com>
2018-06-18 17:41:42 +0530

    Added new models, migrations not done

--- first/models.py
+++ first/models.py
@@ -22,8 +22,11 @@ class Mobile(models.Model):
     OS = models.CharField(max_length = 40)
     Storage = models.CharField(max_length = 40)
 
-    Price = models.DecimalField(max_digits = 7, decimal_places = 2)
+    #Warranty
+    Warranty_Type = models.CharField(max_length = 50)
+    Warranty_Duration = models.CharField(max_length = 70)
 
+    Price = models.DecimalField(max_digits = 7, decimal_places = 2)
 
     def __str__(self):
     	return 'Mobile: {} {}'.format(self.Manufacturer, self.Model_Name)
@@ -49,9 +52,65 @@ class Laptop(models.Model):
     VRAM = models.DecimalField(max_digits = 3, decimal_places = 1)
     Internet_Connectivity = models.TextField(max_length = 140)
 
+    #Warranty
+    Warranty_Type = models.CharField(max_length = 50)
+    Warranty_Duration = models.CharField(max_length = 70)
+
+    Price = models.DecimalField(max_digits = 7, decimal_places = 2)
+
+    def __str__(self):
+        return 'Laptop: {} {}'.format(self.Manufacturer, self.Model_Name)
+
+class Tablet(models.Model):
+    Manufacturer = models.CharField(max_length = 60)
+    Model_Name = models.CharField(max_length = 100)
+
+    #image
+    Image = models.ImageField(default = None)
+
+    #Specifications
+    Display_Size = models.FloatField()
+    CPU = models.CharField(max_length = 100)
+    RAM = models.DecimalField(max_digits = 3, decimal_places = 0)
+    GPU = models.CharField(max_length = 100)
+    Front_Camera = models.DecimalField(max_digits = 3, decimal_places = 1)
+    Rear_Camera = models.DecimalField(max_digits = 3, decimal_places = 1)
+    Weight = models.DecimalField(max_digits = 4, decimal_places = 2)
+    Resolution = models.CharField(max_length = 40)
+    Battery_Rating = models.DecimalField(max_digits = 5, decimal_places = 0)
+    Colour = models.CharField(max_length = 50)
+    SKU = models.CharField(max_length = 40)
+    OS = models.CharField(max_length = 40)
+    Storage = models.CharField(max_length = 40)
+
+    #Warranty
+    Warranty_Type = models.CharField(max_length = 50)
+    Warranty_Duration = models.CharField(max_length = 70)
 
     Price = models.DecimalField(max_digits = 7, decimal_places = 2)
 
 
     def __str__(self):
-        return 'Laptop: {} {}'.format(self.Manufacturer, self.Model_Name)
+        return 'Tablet: {} {}'.format(self.Manufacturer, self.Model_Name)
+
+class Headset(models.Model):
+    Manufacturer = models.CharField(max_length = 60)
+    Model_Name = models.CharField(max_length = 100)
+
+    #Image
+    Image = models.ImageField(default = None)
+
+    #Specifications
+    Colour = models.CharField(max_length = 50)
+    Driver_Size = models.DecimalField(max_digits = 4, decimal_places = 0)
+    Type = models.CharField(max_length = 90)
+    Driver_Type = models.CharField(max_length = 100)
+
+    #Warranty
+    Warranty_Type = models.CharField(max_length = 50)
+    Warranty_Duration = models.CharField(max_length = 70)
+
+    Price = models.DecimalField(max_digits = 7, decimal_places = 2)
+
+    def __str__(self):
+        return 'Headset: {} {}'.format(self.Manufacturer, self.Model_Name)
--- first/views.py
+++ first/views.py
@@ -4,7 +4,11 @@ from . import models
 
 def index(request):
     mobile = models.Mobile.objects.all()
+    laptop = models.Laptop.objects.all()
 
-    args = {'mobile' : mobile}
+    args = {
+        'mobile': mobile,
+        'laptop': laptop
+    }
 
     return render(request, 'first/index.html', args)
