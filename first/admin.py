898418ef274707955fd5ad6434c8e44f2aa159aa
Atharva-29 <atharva0108@gmail.com>
2018-06-21 16:28:58 +0530

    Added migrations

--- first/admin.py
+++ first/admin.py
@@ -4,3 +4,13 @@ from . import models
 admin.site.register(models.Mobile)
 
 admin.site.register(models.Laptop)
+
+admin.site.register(models.Tablet)
+
+admin.site.register(models.Headset)
+
+admin.site.register(models.PowerBank)
+
+admin.site.register(models.MemoryCard)
+
+admin.site.register(models.Charger)
