961f9fbb2c9190a2485e1162ee2978b98b4ad8a7
Atharva-29 <atharva0108@gmail.com>
2018-06-22 15:52:16 +0530

    Images not broken anymore

--- summer2/urls.py
+++ summer2/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import re_path
+from django.conf.urls.static import static 
+from summer2 import settings
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'^', include('first.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
