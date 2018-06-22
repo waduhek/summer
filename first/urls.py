a32871f78507cbd3e8ed40516ad57fc60c2cf7d8
Ryan Noronha <ryannor56a@gmail.com>
2018-06-22 21:26:09 +0530

    Added templates for products and removed unwanted files

--- first/urls.py
+++ first/urls.py
@@ -7,6 +7,6 @@ from . import models
 urlpatterns = [
     re_path(r'^$', views.index, name = 'index'),
     re_path(r'^about-us/$', views.about, name = 'about-us'),
-    re_path(r'^product/laptops/$', ListView.as_view(queryset = models.Laptop.objects.all(), template_name = "first/product-card.html")),
-    re_path(r'^product/laptops/(?P<pk>\d+)/$', DetailView.as_view(model = models.Laptop, template_name = "first/product-page.html")),
+    re_path(r'^product/laptops/$', ListView.as_view(queryset = models.Laptop.objects.all(), template_name = "first/laptop-card.html")),
+    re_path(r'^product/laptops/(?P<pk>\d+)/$', DetailView.as_view(model = models.Laptop, template_name = "first/laptop.html")),
 ]
